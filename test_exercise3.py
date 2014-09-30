319cd32729bfd18aeda42ce78bd75c97ac4c4095
misquit3 <tania.misquitta@mail.utoronto.ca>
2014-09-29 20:33:45 -0400

    fixed assert statments

--- test_exercise3.py
+++ test_exercise3.py
@@ -9,17 +9,16 @@ def test_checksum():
     """
     Inputs that are the correct format and length
     """
-    rock-0, paper 1 scissors 2
-    # check that expected input behaves correctly
+       # check that expected input behaves correctly
     assert decide_rps("Rock", "Rock") == 0
     assert decide_rps("Rock", "Paper") == 1
     assert decide_rps("Rock", "Scissors") == 2
     assert decide_rps("Scissors", "Scissors") == 0
     assert decide_rps("Scissors", "Paper") == 1
     assert decide_rps("Scissors", "Rock") == 2
-    assert decide_rps("Paper", "Scissors") == 0
-    assert decide_rps("Paper", "Paper") == 1
-    assert decide_rps("Paper", "Rock") == 2
+    assert decide_rps("Paper", "Paper") == 0
+    assert decide_rps("Paper", "Scissors") == 2
+    assert decide_rps("Paper", "Rock") == 1
     # check that correct exception is thrown for bad value
     with pytest.raises(ValueError):
         decide_rps("Rock", "Banana") == 1
@@ -28,5 +27,3 @@ def test_checksum():
     with pytest.raises(TypeError):
         decide_rps("Rock", 2) == 1
     # other tests
-
-test_checksum()
